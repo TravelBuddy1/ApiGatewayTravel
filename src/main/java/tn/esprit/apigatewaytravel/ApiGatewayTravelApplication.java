6d0c3ddb233e267efc229c9a42d37f368f5fdbce
rounaaa <raouenem@gmail.com>
2026-02-21 16:47:35 +0100

    ApiGateway

---
+++ src/main/java/tn/esprit/apigatewaytravel/ApiGatewayTravelApplication.java
@@ -0,0 +1,33 @@
+package tn.esprit.apigatewaytravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayTravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayTravelApplication.class, args);
+    }
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("feedback-route", r -> r
+                        .path("/feedback/**")
+                        .uri("lb://feedback"))  // Load balancer + service name
+
+                .route("accommodation-route", r -> r
+                        .path("/accommodation/**")
+                        .uri("lb://accommodation-service"))
+
+                .route("user-route", r -> r
+                        .path("/users/**")
+                        .uri("lb://user"))
+                .build();
+    }
+}
---
+++ src/test/java/tn/esprit/apigatewaytravel/ApiGatewayTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.apigatewaytravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayTravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
